2ccb8dd2ac220c7225441fe5bcf7d675cd2d0676
Emil Stenqvist <emsten@gmail.com>
2013-06-14 21:08:10 +0200

    Finished the arithmetic implementation - still requires a division operator, and a to-decimal function, to display the number in decimal-digit form. Also not fully satisfied with the definition of one and add1.

--- arithmetic.scm
+++ arithmetic.scm
@@ -1,27 +1,45 @@
-;; It is tempting to define zero and one like this:
 ;;
-;; (define zero '())
-;; (define one '(()))
+;; Upon reading The Little Schemer, I was inspired to start defining
+;; the basic arithmetic operations, a bit like they do in the book.
 ;;
-;; However, this doesn't make much sense - since their definitions are
-;; directly connected to the definitions of zero?, add1 and sub1.
-;; Hence their definitions are contained in the following three
-;; pseudo-primitive functions:
+;; The cool idea is that the _representation_ of numbers is arbitrary
+;; to the operation of the program - it only matters for input, output
+;; and efficiency of computation. If one can pin down how numbers are
+;; represented in as little code as possible - everything else can be
+;; define regardless of the representation we choose.
+;;
+;; I played around quiet a bit with the basic most basic primitives
+;; from which the basic arithmetic operations can then be derived.
+;;
+;; At first, I didn't define zero and one. However - this meant I had
+;; to write them out explicitly in many places, which feels wrong. I
+;; ought to read up on some number theory - but after consideration it
+;; made sense to define the following as our primitive operations and
+;; symbols:
+;;
+;; zero, one, add1 and sub1
+;;
 
+
+(define zero '())
 (define zero?
  (lambda (n)
-   (null? n)))
+   (eq? n zero)))
 
 (define add1
   (lambda (n)
+    ;; Not happy with this one - we define the same thing twice
     (cons '() n)))
-
 (define sub1
   (lambda (n)
     (cdr n)))
 
-;; We can now define some common arithmetic operations from the
-;; previous three primitive operations:
+(define one (add1 zero))
+
+;; We now proceed to define some common arithmetic operations, using
+;; _only_ the previously defined operations, and logic (i.e. the cond
+;; form)
+
 (define +
   (lambda (a b)
     (cond
@@ -37,13 +55,44 @@
 (define *
   (lambda (a b)
     (cond
-     ;; Hmm, we explicitly define zero here.
-     ((zero? b) '())
+     ((zero? b) zero)
      (else (+ a (* a (sub1 b)))))))
 
 (define pow
   (lambda (a b)
     (cond
-     ;; And here we specify one, perhaps these ought to be atoms anyway?
-     ((zero? b) '(()))
+     ;; This denotes the agreed-upon convention that any number raised
+     ;; to 0 is 1.
+     ((zero? b) one)
      (else (* a (pow a (sub1 b)))))))
+
+
+;; Functions for creating numbers from a decimal-digit representation.
+
+(define from-digit
+  (lambda (digit)
+    (cond
+     ((eq? digit 0) zero)
+     ((eq? digit 1) (add1 (from-digit 0)))
+     ((eq? digit 2) (add1 (from-digit 1)))
+     ((eq? digit 3) (add1 (from-digit 2)))
+     ((eq? digit 4) (add1 (from-digit 3)))
+     ((eq? digit 5) (add1 (from-digit 4)))
+     ((eq? digit 6) (add1 (from-digit 5)))
+     ((eq? digit 7) (add1 (from-digit 6)))
+     ((eq? digit 8) (add1 (from-digit 7)))
+     ((eq? digit 9) (add1 (from-digit 8))))))
+
+(define decimal-base (add1 (from-digit 9)))
+
+(define from-decimal
+  (lambda (digits)
+    (cond
+     ((null? digits) zero)
+     (else
+      (+ (* (from-digit (car digits))
+            (pow decimal-base
+                 (sub1 (from-digit (length digits)))))
+         (from-decimal (cdr digits)))))))
+
+;; to-decimal requires division and moduli operation, leaving for now.
